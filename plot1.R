14cfc4f1e902cfea0eae035ed468cd736a56c79d
zheyness <lindsey.serato@gmail.com>
2018-03-28 01:09:37 +0800

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 data          <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(data)   <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subsetData    <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
 
 png("plot1.png", width=480, height=480)
